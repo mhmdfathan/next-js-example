adc77c79caed6b18d4d63726ec2d51a667ea1196
Fathan <102466798+mhmdfathan@users.noreply.github.com>
2025-04-23 06:12:45 +0700

    auto deploy test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -18,6 +18,7 @@ export default function Home() {
             Get started by editing <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Auto deploy with Coolify!</li>
         </ol>
 
         <div className={styles.ctas}>
